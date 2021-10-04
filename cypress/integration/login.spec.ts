ae58e99df4eb5d902783cec3dfe59db1d24ca482
Tobias Struckmeier <tobias.struckmeier@adesso.de>
2021-10-04 16:25:49 +0200

    push

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -9,15 +9,27 @@ describe('Login', () => {
     it('should navigate to the login', () => {
         cy.get('app-auth-page').contains('Sign in');
         
-        cy.get('[data-testid=email]').type('testuser@example.com');
+        cy.get('[data-testid=email]').as('email');
+
+        cy.visit('/');
+        cy.contains('Sign in').click();
+
+        //cy.get('[data-testid=email]').type('testuser@example.com');
+
+        cy.get('@email').type('testuser@example.com');
         
         cy.get('[data-testid=password]').type('password');
 
         cy.get('[data-testid=login-button]').click();
 
-        cy.url().should('include', '/');
+        cy.url().should('include', `${Cypress.config().baseUrl}/`);
         
         cy.get('[data-testid=username]').should('contain', 'Testuser');
+
+        cy.contains('.nav-item', 'Your Feed')
+            .find('.nav-link')
+            .should('have.class', 'active');
+
     })
 
 });
