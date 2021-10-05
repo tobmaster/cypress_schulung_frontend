2c48d9d274535def6d9a1f0047aaa871883f30d2
shete <priyanka.shete@adesso.de>
2021-10-05 21:52:24 +0200

    Latest login.spec

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -14,10 +14,25 @@ describe('Login', () => {
         cy.get('[data-testid=password]').type('password');
 
         cy.get('[data-testid=login-button]').click();
+        cy.log('Login Succeded', Cypress.config())
 
         cy.url().should('include', '/');
         
         cy.get('[data-testid=username]').should('contain', 'Testuser');
+        
+        cy.contains('Global Feed').click();
+
+        cy.get('[data-testid=article-preview]').eq(1)
+        .find('h1').should('contain', 'End-To-End Testing leichtgemacht');
+
+        cy.get('[data-testid=article-preview]').eq(1)
+        .find('.tag-list > .tag-pi').as('tags');
+
+        cy.get('@tags').eq(0).should('contain', 'e2e');
+        cy.get('@tags').eq(1).should('contain', 'testing');
+        cy.get('@tags').eq(2).should('contain', 'cypress');
+
+        cy.get('@tags').eq(2).screenshot('filename')
     })
 
 });
