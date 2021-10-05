49559f0ea8a3c0125b6cab0a168dc9856affc814
Tobias Struckmeier <tobias.struckmeier@adesso.de>
2021-10-05 21:22:39 +0200

    Updated for advanced

--- steps/x_shadowdom_clock/clock.spec.ts
+++ steps/x_shadowdom_clock/clock.spec.ts
@@ -1,9 +1,8 @@
 
-describe('toaser', () => {
+describe('toaster', () => {
 
-    it('should toast', () =>  {
-        
-        cy.visit('http://localhost:8080/');
+    it('should toast', () =>  {        
+        cy.visit('https://tobmaster.gitlab.io/tasty-toaster/');
         
         cy.get('tasty-toaster').shadow()
             .as('toaster');
@@ -13,31 +12,28 @@ describe('toaser', () => {
             .invoke("val", 3)
             .trigger("change");
 
-        // cy.clock();
-
         cy.get('@toaster')
             .find('.toaster .handle')
             .click();
-            
-        cy.tick(10000);
-
+     
         cy.get('@toaster')
             .find('.toasts .toast')
             .eq(0, {timeout: 9000})
             .should('have.class','toasted');
 
-        /*cy.clock().then((clock) => {
-            clock.restore()
-        })*/
-
         cy.get('@toaster')
             .find('.toaster .handle')
             .click();
 
-        cy.tick(10000);
-
         cy.get('@toaster')
             .find('.fire .particle').should('have.length.gt', 1);
+        // cy.clock();
+       
+        //cy.tick(10000);
+
+        /*cy.clock().then((clock) => {
+            clock.restore()
+        })*/
 
     })
 
