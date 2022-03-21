dd924675e844409a274e15fd3b2b5a7f3a9741d7
Tobias Struckmeier <github@struckmeier.name>
2022-03-21 12:11:41 +0100

    progress

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,29 +1,29 @@
 /// <reference types="cypress" />
 
-describe('login', () => {
+describe("login", () => {
   beforeEach(() => {
-    cy.visit('/login');
+    cy.visit("/login");
   });
 
-  it('login as example user', () => {
-    cy.get('.auth-page').contains('Sign in');
+  it("login as example user", () => {
+    cy.get(".auth-page").contains("Sign in");
 
-    cy.get('[data-testid="email"]').type('testuser@example.com');
+    cy.get('[data-testid="email"]').type("testuser@example.com");
 
-    cy.get('[data-testid="password"]').type('password');
+    cy.get('[data-testid="password"]').type("password");
 
     cy.get('[data-testid="login-button"]').click();
 
-    cy.get('[data-testid="username"]').should('have.text', 'Test User');
+    cy.get('[data-testid="username"]').should("contain", "Test User");
 
     cy.get('[data-testid="global-feed"]').click();
 
-    cy.get('[data-testid="article-preview"]').should('have.length', 3);
+    cy.get('[data-testid="article-preview"]').should("have.length", 3);
 
     cy.get('[data-testid="article-preview"]')
       .eq(0)
-      .find('.preview-link')
-      .contains('End to end testing leichtgemacht');
+      .find(".preview-link")
+      .contains("End to end testing leichtgemacht");
   });
 });
 
