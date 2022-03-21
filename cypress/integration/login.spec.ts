552297d8c54c78ad368f33f4d0a86e7f157d9ae4
Tobias Struckmeier <github@struckmeier.name>
2022-03-21 11:26:27 +0100

    progress 1

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,1 +1,27 @@
-describe('', () => {});
+/// <reference types="cypress" />
+
+describe("login", () => {
+  beforeEach(() => {
+    cy.visit("/login");
+  });
+
+  it("login as example user", () => {
+    cy.get(".auth-page").contains("Sign in");
+
+    cy.get('[data-testid="email"]').type("testuser@example.com");
+
+    cy.get('[data-testid="password"]').type("password");
+
+    cy.get('[data-testid="login-button"]').click();
+
+    cy.get('[data-testid="username"]', { timeout: 10000 }).should(
+      "have.text",
+      "Test User"
+    );
+  });
+});
+
+// Ingorien
+/*cy.get('[data-testid="username"]').should(($el) => {
+      expect($el).to.contain("Test User");
+    });*/
