16eae9ff4adcf18d9e0ff6b56e841c3a773b1536
Tobias Struckmeier <github@struckmeier.name>
2022-09-20 21:04:38 +0200

    Updated cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,36 +24,36 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import '@testing-library/cypress/add-commands';
+import "@testing-library/cypress/add-commands";
 
 Cypress.Commands.add(
-  'loginByUI',
-  (username = 'testuser@example.com', password = 'password') => {
-    cy.visit('/login');
-    cy.url().should('contain', '/login');
-    cy.get('.auth-page').contains('Sign in');
-    cy.get('[data-testid="email"]').as('emailinput');
-    cy.get('@emailinput').type(username);
+  "loginByUI",
+  (username = "testuser@example.com", password = "password") => {
+    cy.visit("/login");
+    cy.url().should("contain", "/login");
+    cy.get(".auth-page").contains("Sign in");
+    cy.get('[data-testid="email"]').as("emailinput");
+    cy.get("@emailinput").type(username);
     cy.get('[data-testid="password"]').type(password);
     return cy.get('[data-testid="login-button"]').click();
   }
 );
 
-Cypress.Commands.add('loginTestUser', () => {
+Cypress.Commands.add("loginTestUser", () => {
   return cy
     .request({
-      method: 'POST',
-      url: 'http://164.92.132.42:3000/api/users/login',
+      method: "POST",
+      url: "http://vrt.struckmeier.name:3000/api/users/login",
       body: {
         user: {
-          email: 'testuser@example.com',
-          password: 'password',
+          email: "testuser@example.com",
+          password: "password",
         },
       },
     })
     .then((response) => {
       expect(response.status).to.eq(200);
-      expect(response.body.user).to.have.property('token');
-      localStorage.setItem('jwtToken', response.body.user.token);
+      expect(response.body.user).to.have.property("token");
+      localStorage.setItem("jwtToken", response.body.user.token);
     });
 });
