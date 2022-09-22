d84c01bd9b2ce6f6a9d0db83f1a0839cdb20f29d
Tobias Struckmeier <github@struckmeier.name>
2022-09-22 22:03:49 +0200

    Follow Button Component Test

---
+++ src/app/shared/buttons/follow-button.component.cy.ts
@@ -0,0 +1,95 @@
+import { Router } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { BehaviorSubject, of } from "rxjs";
+import { UserService } from "src/app/core";
+import { ProfilesService } from "src/app/core/services/profiles.service";
+import { FollowButtonComponent } from "./follow-button.component";
+
+describe("FollowButtonComponent", () => {
+  let mocks = [];
+  const userProfile = {
+    username: "Sample",
+    bio: "",
+    image: "",
+    following: null,
+  };
+
+  beforeEach(() => {
+    const routerMock = { navigate: () => {} };
+    const profileServiceMock = {
+      follow: (name) => of({ ...userProfile, following: true }),
+      unfollow: (name) => of({ ...userProfile, following: false }),
+    };
+    mocks = [
+      {
+        provide: Router,
+        useValue: routerMock,
+      },
+      {
+        provide: ProfilesService,
+        useValue: profileServiceMock,
+      },
+      {
+        provide: UserService,
+        useValue: {
+          isAuthenticated: new BehaviorSubject<boolean>(true),
+        },
+      },
+    ];
+  });
+
+  it("allow to follow", () => {
+    cy.mount(
+      '<app-follow-button (toggle)="toggle.emit($event)" [profile]="profile"></app-follow-button>',
+      {
+        imports: [RouterTestingModule.withRoutes([])],
+        providers: mocks,
+        componentProperties: {
+          profile: { ...userProfile, following: false },
+          toggle: {
+            emit: cy.spy().as("toggleSpy"),
+          },
+        },
+        declarations: [FollowButtonComponent],
+      }
+    );
+
+    cy.get("button").should(($el) =>
+      expect($el.text().trim()).to.equal("Follow Sample")
+    );
+    cy.get("button").click();
+    cy.get("button").should(($el) =>
+      expect($el.text().trim()).to.equal("Unfollow Sample")
+    );
+
+    cy.get("@toggleSpy").should("have.been.called", 1);
+  });
+
+  it("allow to unfollow", () => {
+    cy.mount(
+      '<app-follow-button (toggle)="toggle.emit($event)" [profile]="profile"></app-follow-button>',
+      {
+        imports: [RouterTestingModule.withRoutes([])],
+        providers: mocks,
+        componentProperties: {
+          profile: { ...userProfile, following: true },
+          toggle: {
+            emit: cy.spy().as("toggleSpy"),
+          },
+        },
+        declarations: [FollowButtonComponent],
+      }
+    );
+
+    cy.get("button").should(($el) =>
+      expect($el.text().trim()).to.equal("Unfollow Sample")
+    );
+
+    cy.get("button").click();
+
+    cy.get("button").should(($el) =>
+      expect($el.text().trim()).to.equal("Follow Sample")
+    );
+    cy.get("@toggleSpy").should("have.been.called", 1);
+  });
+});
