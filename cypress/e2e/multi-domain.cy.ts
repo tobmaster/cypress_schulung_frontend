e45b0d8d4f10a3004f82a16b16818696f7c479c6
Tobias Struckmeier <github@struckmeier.name>
2022-09-25 20:17:46 +0200

    Pass over

--- cypress/e2e/multi-domain.cy.ts
+++ cypress/e2e/multi-domain.cy.ts
@@ -3,16 +3,11 @@
 import { loginAs } from "../helper/login";
 
 describe("login", () => {
-  beforeEach(() => {
-    cy.visit("/login");
-  });
+  beforeEach(() => {});
 
   it("should be able to log in", () => {
     // check if log in page is correct
-    cy.visit("https://www.cypress.io");
-    cy.contains("Sign in");
-
-    cy.viewport("macbook-16");
+    //cy.visit("https://www.cypress.io");
 
     cy.origin("https://search.brave.com", () => {
       cy.visit("/");
@@ -21,9 +16,6 @@ describe("login", () => {
       cy.get("#searchbox").type("{enter}");
       cy.contains("JavaScript End to End Testing Framework").click();
     });
-    cy.origin("https://www.cypress.io", () => {
-      cy.contains("The web has evolved. Finally, testing has too.").click();
-    });
-    cy.pause();
+    //  cy.contains("The web has evolved. Finally, testing has too.").click();
   });
 });
