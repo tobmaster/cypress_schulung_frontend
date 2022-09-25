79a80cd919292fd283b2cb745cffed627cea2f09
Tobias Struckmeier <github@struckmeier.name>
2022-09-25 20:04:17 +0200

    Brave search

--- cypress/e2e/multi-domain.cy.ts
+++ cypress/e2e/multi-domain.cy.ts
@@ -4,19 +4,26 @@ import { loginAs } from "../helper/login";
 
 describe("login", () => {
   beforeEach(() => {
-    cy.visit('/login');
+    cy.visit("/login");
   });
 
-  it('should be able to log in', () => {
+  it("should be able to log in", () => {
     // check if log in page is correct
-    cy.contains('Sign in');
+    cy.visit("https://www.cypress.io");
+    cy.contains("Sign in");
 
-    cy.viewport('macbook-16');
-    cy.origin('https://www.ecosia.org', () => {
-      cy.visit('/')
-      cy.get('.search-form__input').type('Cypress');
-      cy.get('.search-form__input').type('{enter}');
-      cy.pause()
-    })
+    cy.viewport("macbook-16");
+
+    cy.origin("https://search.brave.com", () => {
+      cy.visit("/");
+
+      cy.get("#searchbox").type("Cypress");
+      cy.get("#searchbox").type("{enter}");
+      cy.contains("JavaScript End to End Testing Framework").click();
+    });
+    cy.origin("https://www.cypress.io", () => {
+      cy.contains("The web has evolved. Finally, testing has too.").click();
+    });
+    cy.pause();
   });
 });
