168a367a172451841851b80408cc7c905d96f853
Tobias Struckmeier <github@struckmeier.name>
2022-10-01 18:14:50 +0200

    Cleanup - Ready for workshop

--- cypress/helper/delay.ts
+++
@@ -1,9 +0,0 @@
-export const delayLogin = (timedelay = 4100) => {
-  cy.log("DELAYED LOGIN", timedelay);
-
-  cy.intercept({ url: "**/api/users/login", middleware: true }, (req) => {
-    req.on("response", (res) => {
-      res.setDelay(4100);
-    });
-  });
-};
