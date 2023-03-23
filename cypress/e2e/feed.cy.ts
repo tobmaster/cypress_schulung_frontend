0646740a7d6c2c47ce4e3540b007a73b62c7b5dc
Tobias Struckmeier <github@struckmeier.name>
2023-03-23 14:40:06 +0100

    login

--- cypress/e2e/feed.cy.ts
+++ cypress/e2e/feed.cy.ts
@@ -1,30 +1,30 @@
-describe('empty spec', () => {
-  beforeEach(() => {
-    const email = "testuser@example.com";
+import { login } from "../helper/login";
 
-    cy.visit("/login");
-    cy.url().should("contain", "/login");
-    cy.get(".auth-page").contains("Sign in");
-    cy.get('[data-testid="email"]').type(email);
-    cy.get('[data-testid="password"]').type("password{enter}");
-    cy.get('[data-testid="username"]').should("contain", "Test User");
-  })
+describe("empty spec", () => {
+    beforeEach(() => {
+        login("testuser@example.com", "password", "Test User");
+    });
 
-  it('should show your feed', () => {
-    cy.intercept('**/articles/feed?**')
-        .as('yourFeed');
-    cy.visit('/');
+    it("should show your feed", () => {
+        cy.intercept("**/articles/feed?**").as("yourFeed");
+        cy.visit("/");
 
-    cy.wait('@yourFeed').then((feedRes) => {
-      cy.get('[data-testid=article-preview]')
-          .should('have.length', feedRes.response.body.articles.length);
+        cy.wait("@yourFeed").then((feedRes) => {
+            cy.get("[data-testid=article-preview]").should(
+                "have.length",
+                feedRes.response.body.articles.length
+            );
 
-      feedRes.response.body.articles.forEach((article, index) => {
-        cy.get('[data-testid=article-preview]').eq(index).find('h1')
-            .should('contain', article.title);
-        cy.get('[data-testid=article-preview]').eq(index).find('p')
-            .should('contain', article.description);
-      });
-    })
-  })
-})
+            feedRes.response.body.articles.forEach((article, index) => {
+                cy.get("[data-testid=article-preview]")
+                    .eq(index)
+                    .find("h1")
+                    .should("contain", article.title);
+                cy.get("[data-testid=article-preview]")
+                    .eq(index)
+                    .find("p")
+                    .should("contain", article.description);
+            });
+        });
+    });
+});
