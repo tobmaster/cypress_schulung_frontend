a094f5779daad2667c8ae4f536a6340cd36192f1
hannah <hannah.ebert@adesso.de>
2023-03-27 00:34:59 +0200

    last commit for today

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: "http://localhost:4200",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "src/**/*.cy.ts",
   },
 });
