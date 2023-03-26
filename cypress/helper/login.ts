1b80cb96f6f49eb6e345d5df931662c9af77caba
hannah <hannah.ebert@adesso.de>
2023-03-26 19:16:10 +0200

    rdy 4 workshop

--- cypress/helper/login.ts
+++
@@ -1,8 +0,0 @@
-export function login(email, password, username) {
-    cy.visit("/login");
-    cy.url().should("contain", "/login");
-    cy.get(".auth-page").contains("Sign in");
-    cy.get('[data-testid="email"]').type(email);
-    cy.get('[data-testid="password"]').type(password + "{enter}");
-    cy.get('[data-testid="username"]').should("contain", username);
-}
