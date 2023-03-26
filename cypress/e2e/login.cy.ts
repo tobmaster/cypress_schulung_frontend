f6d2cae09a217bd4cea52c5677554440f0a473d7
hannah <hannah.ebert@adesso.de>
2023-03-26 22:16:17 +0200

    stand 2

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,26 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+
+    it('passes', () => {
+        cy.visit('http://localhost:4200/login');
+
+        cy.url().should('eq', `${Cypress.config('baseUrl')}/login`);
+
+        cy.get('[data-testid="login-form"]')
+            .should('contain', 'Sign in');
+
+        cy.get('[data-testid="email"]')
+            .type("testuser@example.com");
+        cy.get('[data-testid="password"]')
+            .type("password{enter}");
+
+        cy.get('[data-testid="username"]')
+            .should('contain', 'Test User');
+
+        cy.get('[data-testid="your-feed"]')
+            .should('have.class', 'active');
+
+        /*       cy.get('[data-testid="login-button"]')
+                   .click();*/
+
+    })
 })
