520cee07917f9adf1ebbeb5cb0de0841cabd4bbf
Tobias Struckmeier <tobias.struckmeier@adesso.de>
2023-03-27 10:39:25 +0200

    Stuff

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login', () => {
+
+  it('should be possible to login', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('.banner > .container > .logo-font')
+      .should('contain', 'conduit')
   })
+
 })
