e8927d646f613d92983eba6ced0c6f737c4047a7
hannah <hannah.ebert@adesso.de>
2023-03-27 12:03:52 +0200

    login improved

---
+++ cypress/e2e/login_improved.cy.ts
@@ -0,0 +1,43 @@
+describe('Login', () => {
+
+    beforeEach(() => {
+        cy.visit('/login');
+    });
+
+    it('should be possible to login', () => {
+        cy.get('.auth-page').contains('Sign in');
+
+        cy.get('[data-testid="email"]')
+            .type('testuser@example.com');
+
+        cy.get('[data-testid="password"]')
+            .type('password');
+
+
+        cy.get('[data-testid="login-button"]')
+            .click();
+
+        cy.url().should('eq', `${Cypress.config().baseUrl}/`);
+
+        cy.get('[data-testid="your-feed"]').should('have.class', 'inactive');
+
+
+        /** Here **/
+        cy.get('[data-testid=article-preview]').eq(1).as('article'); //alias
+
+        cy.get('@article')
+            .find('.tag-list > .tag-pill')
+            .as('tags')
+
+        cy.get('@article')
+            .find('.preview-link')
+            .contains('End-To-End Testing leichtgemacht');
+
+        cy.get('@tags').eq(0)
+            .should('contain', 'e2e');
+        cy.get('@tags').eq(1)
+            .should('contain', 'testing');
+        cy.get('@tags').eq(2)
+            .should('contain', 'cypress');
+    })
+})
